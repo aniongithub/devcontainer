6c30af5247ea61735f312387f13f2e6794aff2f3
aniongithub <ani@anionline.me>
2021-05-15 19:32:15 -0700

    Artifact cleanup + better naming for nvidia-specific template

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # Build our container with tools required
 docker build -t devcontainer-packager .
 
+# Clean any previous build artifacts
+rm -f devcontainer_*.*
+
 # Pipe commands from our packaging script to the container we just built
 cat build-package.sh | docker run --rm -i -v $PWD:/devcontainer devcontainer-packager /bin/bash
